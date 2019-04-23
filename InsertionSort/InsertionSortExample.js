4224d92b740272d2afd19b9cd6c34cd65d54f556
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-23 16:58:15 +0200

    Update InsertionSortExample.js

--- InsertionSort/InsertionSortExample.js
+++ InsertionSort/InsertionSortExample.js
@@ -1,1 +1,23 @@
+let sortableArray = [];
 
+for(let i = 0; i < 10; i++) {
+  const randomNumb = Math.floor(Math.random() * 1000);
+  sortableArray.push(randomNumb);
+}
+const sortedArray = InsertionSort(sortableArray);
+console.log(sortedArray);
+
+function InsertionSort(array) {
+  const length = array.length; 
+  
+  for(let i = 1; i < length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while(j >= 0 && key < array[j]) {
+      array[j + 1] = array[j];
+      j -= 1;
+    }
+    array[j + 1] = key;
+  }   
+  return array;
+}
