bca03e4496da945b72ec07bb7a6803541c2f29fc
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-23 14:20:05 +0200

    Update BubbleSortExample.js

--- BubbleSort/BubbleSortExample.js
+++ BubbleSort/BubbleSortExample.js
@@ -1,1 +1,26 @@
+let sortableArray = [];
 
+for(let i = 0; i < 1000; i++) {
+  const randomNumb = Math.floor(Math.random() * 1000);
+  sortableArray.push(randomNumb);
+}
+
+let sortedArray = BubbleSort(sortableArray);
+console.log(sortedArray);
+
+function BubbleSort(array) {
+  const length = array.length;
+  for(i = 0; i < length; i++) {
+    let swapped = false;
+    
+    for(j = 0; j <  length - i - 1; j++) {
+      if(array[j] > array[j+1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]];
+        swapped = true;
+      }
+    }
+    if(swapped === false) {
+      return array; 
+    } 
+  }
+}
