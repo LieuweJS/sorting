cd142b2a50cfdaa9d4a8b9724105a6751021acff
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-23 14:20:31 +0200

    Update BubbleSortFunction.js

--- BubbleSort/BubbleSortFunction.js
+++ BubbleSort/BubbleSortFunction.js
@@ -1,1 +1,16 @@
-
+function BubbleSort(array) {
+  const length = array.length;
+  for(i = 0; i < length; i++) {
+    let swapped = false;
+    
+    for(j = 0; j <  length - i - 1; j++) {
+      if(array[j] > array[j+1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]];
+        swapped = true;
+      }
+    }
+    if(swapped === false) {
+      return array; 
+    } 
+  }
+}
