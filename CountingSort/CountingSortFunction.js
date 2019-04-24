a732614933241280d133bfea0350cccae0a15a30
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-24 16:53:20 +0200

    Update CountingSortFunction.js

--- CountingSort/CountingSortFunction.js
+++ CountingSort/CountingSortFunction.js
@@ -1,1 +1,21 @@
+function CountingSort(array) {
+  let i = 0;
+  let j = 0;
+  const length = array.length;
+  let count = [];
+  for (i; i <= length + 1; i++) {
+    count[i] = 0;
+  }
+  for (i = 0; i < length; i++) {
+    count[array[i]] += 1;
+  }
+  for (i = 0; i <= length + 1; i++) {
 
+    while (count[i] > 0) {
+      array[j] = i;
+      j++;
+      count[i]--;
+    }
+  }
+  return array;
+}
