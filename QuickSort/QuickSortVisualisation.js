5e3db85dcd192c27f93425d678cad9d70b2f0646
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-24 15:08:01 +0200

    Update QuickSortVisualisation.js

--- QuickSort/QuickSortVisualisation.js
+++ QuickSort/QuickSortVisualisation.js
@@ -2,64 +2,63 @@ const sortableArray = [];
 let i = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
   colorMode(HSB);
-  for (let i = 0; i < width; i++) {
+  for (let i = 0; i < width / 2; i++) {
     sortableArray.push(Math.floor(Math.random() * 360) + 1);
   }
+  QuickSort(sortableArray, 0, (sortableArray.length - 1));
 }
 
 function draw() {
   background(0);
-  QuickSort(sortableArray, 0, sortableArray.length-1);
   for (let i = 0; i < sortableArray.length; i++) {
     noStroke();
     fill(sortableArray[i], 100, 100);
-    rect(i, 0, 1, height);
+    //rect(i*2, 0, 2, height);
+    //fill(255)
+    rect(i * 2, (height - sortableArray[i] * height / 360), 2, (sortableArray[i] * (height / 360)));
   }
 }
 
-
-
-
-
-
 async function QuickSort(array, low, high) {
-   const length = array.length; 
-   let PartitionIndex;
+  const length = array.length;
+  let PartitionIndex;
 
-
-  if(low < high) {
+  if (low < high) {
     let PartitionIndex = await Partition(array, low, high);
+
     await Promise.all([
-    //changes the high closer to the middle
-    QuickSort(array, low, PartitionIndex - 1),
-    //changes the low closer to the middle    
-    QuickSort(array, PartitionIndex + 1, high)])
-  }  
+      QuickSort(array, low, PartitionIndex - 1),
+      QuickSort(array, PartitionIndex + 1, high)
+    ])
+  }
   return array;
 }
-        
+
 
 async function Partition(array, low, high) {
-   let PartitionIndex = low;
-   await sleep(200)
-   for(let i = low; i < high; i++) {
-    if(array[i] < array[high]){
-      const temp1 = array[i];
-      array[i] = array[PartitionIndex];
-      array[PartitionIndex] = temp1;
-      
+  let PartitionIndex = low;
+  for (let i = low; i <= high; i++) {
+    if (array[i] < array[high]) {
+      await swap(array, i, PartitionIndex)
+
       PartitionIndex++;
     }
   }
-  const temp2 = array[high];
-  array[high] = array[PartitionIndex];
-  array[PartitionIndex] = temp2;
-  
+  await swap(array, high, PartitionIndex)
+
   return PartitionIndex;
-}  
+}
+
+async function swap(array, a, b) {
+  await sleep(15)
+  const temp = array[a];
+  array[a] = array[b]
+  array[b] = temp
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
