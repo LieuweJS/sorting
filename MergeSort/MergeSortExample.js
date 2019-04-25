8cd87b3a88cd3eefc8c602d40eaaf9e3cd20b1b5
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-25 18:44:47 +0200

    Update MergeSortExample.js
    Changed merge sort algorithm from a recursive sort to a non-recursive sort.

--- MergeSort/MergeSortExample.js
+++ MergeSort/MergeSortExample.js
@@ -6,37 +6,40 @@ for (let i = 0; i <= 10; i++) {
 
 console.log(MergeSort(array));
 
-function MergeSort(array) {
-  if (array.length > 1) {
-    const middle = array.length / 2;
-
-    const left = array.slice(0, middle);
-    const right = array.slice(middle);
-
-    const leftResult = MergeSort(left);
-    const rightResult = MergeSort(right);
-
-    result = merge(leftResult, rightResult);
-
-    return result;
-  } else {
-    return array;
-  }
-}
-
 function merge(left, right) {
-  let indexLeft = 0;
-  let indexRight = 0;
   let array = [];
-
-  while (indexLeft < left.length && indexRight < right.length) {
-    if (left[indexLeft] < right[indexRight]) {
-      array.push(left[indexLeft]);
-      indexLeft++;
+  while (left.length > 0 && right.length > 0) {
+    if (left[0] < right[0]) {
+      array.push(left.shift());
     } else {
-      array.push(right[indexRight]);
-      indexRight++;
+      array.push(right.shift());
     }
   }
-  return array.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+  array = array.concat(left).concat(right);
+  left.splice(0, left.length);
+  right.splice(0, right.length);
+
+  return array;
+}
+
+function MergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+  let result = [];
+  let length;
+  let i;
+  let j;
+
+  for (i = 0, length = array.length; i < length; i++) {
+    result.push([array[i]]);
+  }
+  result.push([]);
+  for (let limit = length; limit > 1; limit = Math.floor((limit + 1) / 2)) {
+    for (j = 0, k = 0; k < limit; j++, k += 2) {
+      result[j] = merge(result[k], result[k + 1]);
+    }
+    result[j] = [];
+  }
+  return result[0];
 }
