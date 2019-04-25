f5d21e8bcb3a134569fad0ac0c28519f86ad8c6f
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-25 14:58:33 +0200

    Create MergeSortExample.js

---
+++ MergeSort/MergeSortExample.js
@@ -0,0 +1,42 @@
+let array = [];
+
+for (let i = 0; i <= 10; i++) {
+  array.push(Math.floor(Math.random() * 1000) + 1);
+}
+
+console.log(MergeSort(array));
+
+function MergeSort(array) {
+  if (array.length > 1) {
+    const middle = array.length / 2;
+
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    const leftResult = MergeSort(left);
+    const rightResult = MergeSort(right);
+
+    result = merge(leftResult, rightResult);
+
+    return result;
+  } else {
+    return array;
+  }
+}
+
+function merge(left, right) {
+  let indexLeft = 0;
+  let indexRight = 0;
+  let array = [];
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      array.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      array.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+  return array.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+}
